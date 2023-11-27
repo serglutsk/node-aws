0dfdd83d5e15c02cdf3b37bb8be94f67c5c16696
serhiipanasiuk <serhii.panasiuk@vaimo.com>
2023-11-27 16:38:20 +0000

    initial

---
+++ app/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.APP_PORT ?? 3000
+
+app.get('/', function (req, res) {
+    res.send(`Hello Serhii. yes, YES YeS!!!`)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running http://localhost:${PORT}`)
+})
